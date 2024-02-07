e2cbd1d429ab9b52236afafb34cf51afdf9b7fa6
jihacshnu <jishnuchowdhury30@gmail.com>
2024-02-07 20:30:42 +0530

    animation added

---
+++ script.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".head", { origin: "top" });
+ScrollReveal().reveal(".bottom", { origin: "bottom" });
+ScrollReveal().reveal(".hero, .left", { origin: "left" });
+ScrollReveal().reveal(".right", { origin: "right" });
